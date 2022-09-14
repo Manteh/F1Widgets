13eb601b6e5cc3c3cf29127ac0eb84954b36365e
Mantas Simanauskas <mantas.simanauskas@ikano.se>
2022-09-14 21:20:11 +0200

    Initial Commit

---
+++ F1Widgets/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  F1Widgets
+//
+//  Created by Mantas Simanauskas on 2022-09-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ F1Widgets/F1WidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  F1WidgetsApp.swift
+//  F1Widgets
+//
+//  Created by Mantas Simanauskas on 2022-09-14.
+//
+
+import SwiftUI
+
+@main
+struct F1WidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
